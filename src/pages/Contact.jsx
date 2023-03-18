ecddf051b7a1c9eacd6547577b74f766c71c8793
Koji Taniguchi <64213053+kojitani@users.noreply.github.com>
2023-03-18 23:12:06 +0900

    added netlify integrated contact us form handling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Contact() {
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log('Form successfully submitted'))
+      .catch(error => alert(error));
+  };
+
+  useEffect(() => {
+    document.querySelector('form').addEventListener('submit', handleSubmit);
+  }, []);
   return (
-    <div className="contact-container container">This is contact page.</div>
+    <div className="contact-container container">
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{' '}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
   );
 }
