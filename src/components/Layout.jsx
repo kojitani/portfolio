71571ba92aaf2fbf189d863a3f34e078932c5e32
Koji Taniguchi <64213053+kojitani@users.noreply.github.com>
2023-03-22 10:40:28 +0900

    test

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,23 +5,23 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 export default function Layout() {
   const location = useLocation();
-  // const waveBackgrounds = [
-  //   '/wave-about.svg',
-  //   '/wave-contact.svg',
-  //   '/wave-home.svg',
-  //   '/wave-projects.svg',
-  // ];
-  // const projectImages = [
-  //   '/images/BrainDump.webp',
-  //   '/images/MinimaList.webp',
-  //   '/images/VanVenture.webp',
-  // ];
-  // const backgroundElements = waveBackgrounds.map((background, i) => {
-  //   return <img key={i} src={background} style={{ display: 'none' }}></img>;
-  // });
-  // const projectImgElements = projectImages.map((img, i) => {
-  //   return <img key={i} src={img} style={{ display: 'none' }}></img>;
-  // });
+  const waveBackgrounds = [
+    '/wave-about.svg',
+    '/wave-contact.svg',
+    '/wave-home.svg',
+    '/wave-projects.svg',
+  ];
+  const projectImages = [
+    '/images/BrainDump.webp',
+    '/images/MinimaList.webp',
+    '/images/VanVenture.webp',
+  ];
+  const backgroundElements = waveBackgrounds.map((background, i) => {
+    return <img key={i} src={background} style={{ display: 'none' }}></img>;
+  });
+  const projectImgElements = projectImages.map((img, i) => {
+    return <img key={i} src={img} style={{ display: 'none' }}></img>;
+  });
 
   return (
     <div>
@@ -30,8 +30,8 @@ export default function Layout() {
         <Outlet context={location} />
       </AnimatePresence>
       <Footer />
-      {/* {backgroundElements}
-      {projectImgElements} */}
+      {backgroundElements}
+      {projectImgElements}
     </div>
   );
 }
